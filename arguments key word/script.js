d6e529e22ea654d5fc5b78e019888b88a8379a33
Mahmood <pejvakco@gmail.com>
2021-02-02 03:17:50 +0100

    Update: arguments key word

--- arguments key word/script.js
+++ arguments key word/script.js
@@ -9,7 +9,7 @@ function func1(a, b, c) {
   // expected output: 3
 }
 func1(1, 2, 3);
-
+//----------------------------------------------------------------
 function longestString() {
   var longest = '';
   for (var i=0; i < arguments.length; i++) {
@@ -23,7 +23,7 @@ function longestString() {
 
 }
 longestString("ali","taghi","reza");
-
+//----------------------------------------------------------------
 function myConcat(separator) {
   let args = Array.prototype.slice.call(arguments, 1);
   return args.join(separator);
@@ -34,8 +34,15 @@ myConcat(', ', 'red', 'orange', 'blue');
 myConcat('; ', 'elephant', 'giraffe', 'lion', 'cheetah');
 // returns "sage. basil. oregano. pepper. parsley"
 myConcat('. ', 'sage', 'basil', 'oregano', 'pepper', 'parsley');
-
+//----------------------------------------------------------------
 function employeeList(...employee){
   console.log(Array.from(arguments));
 }
 employeeList("emp1","emp2","emp3","emp4","emp5")
+//----------------------------------------------------------------
+function marry2(...args) {
+  console.log(args)
+  console.log(Array.from(arguments))
+  console.log(`${arguments[0]} is now married to ${arguments[1]}`)
+}
+marry2('Tim', 'Tina')
