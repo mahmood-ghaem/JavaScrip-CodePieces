cff681cc1fa7f7e619b60b7887bb876a11bd6c65
Mahmood <pejvakco@gmail.com>
2021-02-03 00:39:51 +0100

    Add: IIFE

---
+++ 06-IIFE/script.js
@@ -0,0 +1,30 @@
+// Ways to create IIFE
+
+(function() {
+  alert("Parentheses around the function");
+})();
+
+(function() {
+  alert("Parentheses around the whole thing");
+}());
+
+!function() {
+  alert("Bitwise NOT operator starts the expression");
+}();
+
++function() {
+  alert("Unary plus starts the expression");
+}();
+//-------------------------------------------------------
+(function () {
+    var aName = "Barry";
+})();
+// Variable aName is not accessible from the outside scope
+aName // throws "Uncaught ReferenceError: aName is not defined"
+//-------------------------------------------------------
+var result = (function () {
+    var name = "Barry";
+    return name;
+})();
+// Immediately creates the output:
+result; // "Barry"
