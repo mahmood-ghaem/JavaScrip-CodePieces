3b4292abc7cdfe505bb3d11114f9820972ced16d
Mahmood <pejvakco@gmail.com>
2021-02-02 03:09:50 +0100

    Update: arguments key word

--- arguments key word/script.js
+++ arguments key word/script.js
@@ -1,3 +1,15 @@
+function func1(a, b, c) {
+  console.log(arguments[0]);
+  // expected output: 1
+
+  console.log(arguments[1]);
+  // expected output: 2
+
+  console.log(arguments[2]);
+  // expected output: 3
+}
+func1(1, 2, 3);
+
 function longestString() {
   var longest = '';
   for (var i=0; i < arguments.length; i++) {
@@ -11,6 +23,17 @@ function longestString() {
 
 }
 longestString("ali","taghi","reza");
+
+function myConcat(separator) {
+  let args = Array.prototype.slice.call(arguments, 1);
+  return args.join(separator);
+}
+// returns "red, orange, blue"
+myConcat(', ', 'red', 'orange', 'blue');
+// returns "elephant; giraffe; lion; cheetah"
+myConcat('; ', 'elephant', 'giraffe', 'lion', 'cheetah');
+// returns "sage. basil. oregano. pepper. parsley"
+myConcat('. ', 'sage', 'basil', 'oregano', 'pepper', 'parsley');
 
 function employeeList(...employee){
   console.log(Array.from(arguments));
