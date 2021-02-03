563ae64d173bc0994a3c201f9908a5cb76722387
Mahmood <pejvakco@gmail.com>
2021-02-03 13:18:25 +0100

    Update: call apply bind

--- 08-call apply bind/script.js
+++ 08-call apply bind/script.js
@@ -39,3 +39,16 @@ function getMaxNumber(arr) {
 }
 
 console.log(getMaxNumber(array));
+
+//-----------------------------------------------------
+
+//bind() and currying
+function multiply(a, b) {
+  return a * b;
+}
+
+var multipleByTwo = multiply.bind(this, 2);
+console.log(multipleByTwo(4));
+
+var multipleByThree = multiply.bind(this, 3);
+console.log(multipleByThree(4));
