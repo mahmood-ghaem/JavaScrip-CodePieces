f13c6989cd8a6ca374147f159acfd20a0217cd74
Mahmood <pejvakco@gmail.com>
2021-02-03 13:36:12 +0100

    Update: this Keyword

--- 07-this Keyword/script.js
+++ 07-this Keyword/script.js
@@ -97,3 +97,56 @@ const obj = {
   },
 };
 obj.sing()();
+
+//-----------------------------------------------------
+
+var b = {
+  name: 'jay',
+  say() {
+    console.log(this);
+  },
+};
+
+var c = {
+  name: 'jay',
+  say() {
+    return function () {
+      console.log(this);
+    };
+  },
+};
+
+var d = {
+  name: 'jay',
+  say() {
+    return () => console.log(this);
+  },
+};
+
+console.log(b.say());
+console.log(c.say()());
+console.log(d.say()());
+
+//--------------------------------------------------------
+//test:
+const character = {
+  name: 'Simon',
+  getCharacter() {
+    return this.name;
+  },
+};
+const giveMeTheCharacterNOW = character.getCharacter;
+
+//How Would you fix this?
+console.log('?', giveMeTheCharacterNOW()); //this should return 'Simon' bud doesn't
+
+//solution:
+const character = {
+  name: 'Simon',
+  getCharacter() {
+    return this.name;
+  },
+};
+const giveMeTheCharacterNOW = character.getCharacter.bind(character);
+
+console.log('?', giveMeTheCharacterNOW());
