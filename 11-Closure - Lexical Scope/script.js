e7ffca597a06be166d8de89320208ed140e00198
Mahmood <pejvakco@gmail.com>
2021-02-04 18:26:08 +0100

    Update: Closure - Lexical Scope

--- 11-Closure - Lexical Scope/script.js
+++ 11-Closure - Lexical Scope/script.js
@@ -69,3 +69,14 @@ outerFunction();
 // ney vatsa
 //shashank jha
 //huda
+
+//----------------------------------------------------------------------
+
+function callMeMaybe() {
+  const callMe = 'Hi!';
+  setTimeout(function () {
+    console.log(callMe);
+  }, 4000);
+}
+
+callMeMaybe();
