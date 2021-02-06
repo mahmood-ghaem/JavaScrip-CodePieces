75923f2a88871c5c001ac06ce40415f343b1cab7
Mahmood <pejvakco@gmail.com>
2021-02-06 16:41:29 +0100

    Updated: Arguments Keyword - add some comments

--- 01-Arguments Keyword/script.js
+++ 01-Arguments Keyword/script.js
@@ -9,10 +9,12 @@ function func1(a, b, c) {
   // expected output: 3
 }
 func1(1, 2, 3);
+
 //----------------------------------------------------------------
+
 function longestString() {
   var longest = '';
-  for (var i=0; i < arguments.length; i++) {
+  for (var i = 0; i < arguments.length; i++) {
     if (arguments[i].length > longest.length) {
       longest = arguments[i];
     }
@@ -20,11 +22,14 @@ function longestString() {
   console.log(longest);
   debugger;
   return longest;
-
 }
-longestString("ali","taghi","reza");
+longestString('ali', 'taghi', 'reza');
+
 //----------------------------------------------------------------
+
 function myConcat(separator) {
+  //separator get first item pass to function
+  //args is an array created by some arguments
   let args = Array.prototype.slice.call(arguments, 1);
   return args.join(separator);
 }
@@ -34,15 +39,23 @@ myConcat(', ', 'red', 'orange', 'blue');
 myConcat('; ', 'elephant', 'giraffe', 'lion', 'cheetah');
 // returns "sage. basil. oregano. pepper. parsley"
 myConcat('. ', 'sage', 'basil', 'oregano', 'pepper', 'parsley');
+
 //----------------------------------------------------------------
-function employeeList(...employee){
+
+function employeeList(...employee) {
   console.log(Array.from(arguments));
 }
-employeeList("emp1","emp2","emp3","emp4","emp5")
+employeeList('emp1', 'emp2', 'emp3', 'emp4', 'emp5');
+
 //----------------------------------------------------------------
+
 function marry2(...args) {
-  console.log(args)
-  console.log(Array.from(arguments))
-  console.log(`${arguments[0]} is now married to ${arguments[1]}`)
+  console.log(args);
+  console.log(Array.from(arguments));
+  console.log(`${arguments[0]} is now married to ${arguments[1]}`);
 }
-marry2('Tim', 'Tina')
+marry2('Tim', 'Tina');
+
+// different between (...args) and (arg):
+// first create an array with arguments
+// second create a variable from first argument and put to arg
