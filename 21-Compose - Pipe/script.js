f072a5a0dd3de1908893ab7ef9e66cedc89ba8b2
Mahmood <pejvakco@gmail.com>
2021-03-05 23:24:57 +0100

    update 21

--- 21-Compose - Pipe/script.js
+++ 21-Compose - Pipe/script.js
@@ -3,7 +3,11 @@
 //compose(fn1, fn2, fn3)(50) //Right to lext
 //pipe(fn3, fn2, fn1)(50)//left to right
 
-const compose = (f, g) => (a) => f(g(a))
-const pipe = (f, g) => (a) => g(f(a))
-const multiplyBy3AndAbsolute = compose((num) => num*3, Math.abs)
-console.log(multiplyBy3AndAbsolute(-50))
+const compose = (f, g) => (data) => f(g(data));
+const pipe = (f, g) => (data) => g(f(data));
+//const multiplyBy3AndAbsolute = compose((num) => num * 3, Math.abs);
+const multiplyBy3 = (num) => num * 3;
+const makePositive = (num) => Math.abs(num);
+const multiplyBy3AndAbsolute = compose(multiplyBy3, makePositive);
+
+console.log(multiplyBy3AndAbsolute(-50));
