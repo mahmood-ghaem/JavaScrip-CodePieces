1333ecc5d088fd432311909caaf8221a317ff4bd
Mahmood <pejvakco@gmail.com>
2021-03-14 23:20:30 +0100

    Update Asynchronous

--- 22-Asynchronous/script.js
+++ 22-Asynchronous/script.js
@@ -74,3 +74,36 @@ console.log('4', 'is a crowd');
 // 3 hi
 // 1 is the loneliest number
 // 2 can be as bad as one
+
+//=================================================================
+
+const promisify = (item, delay) =>
+  new Promise((resolve) => setTimeout(() => resolve(item), delay));
+
+const a = () => promisify('a', 100);
+const b = () => promisify('b', 5000);
+const c = () => promisify('c', 3000);
+
+async function parallel() {
+  const promises = [a(), b(), c()];
+  const [output1, output2, output3] = await Promise.all(promises);
+  return `parallel is done: ${output1} ${output2} ${output3}`;
+}
+
+async function race() {
+  const promises = [a(), b(), c()];
+  const output1 = await Promise.race(promises);
+  return `race is done: ${output1}`;
+}
+
+async function sequence() {
+  const output1 = await a();
+  const output2 = await b();
+  const output3 = await c();
+  return `sequence is done ${output1} ${output2} ${output3}`;
+}
+
+sequence().then(console.log);
+parallel().then(console.log);
+race().then(console.log);
+//race().then((data) => console.log(data)); same as top line
